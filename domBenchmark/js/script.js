087fe2d71d8e8ecef8d526019d5fb37fe2cd8753
Toni Alatalo <toni@playsign.net>
2013-11-13 14:49:45 +0200

    optimize dom elem creation by adding to document.body only after all elements are done. refactor dom & pure js entity creation odes to own funcs for clarity.

--- domBenchmark/js/script.js
+++ domBenchmark/js/script.js
@@ -1,103 +1,118 @@
-/* -*- js-indent-level: 8 -*- */
+/* -*- js-indent-level: 4 -*- */
 
 var do_dom = true;
 
 // MUTATIONOBSERVER
 // create an observer instance
 var observer = new MutationObserver(function(mutations) {
-	var start = Date.now();
-	mutations.forEach(function(mutation) {
-		// console.log(mutation.type);
-	});
-	var end = Date.now();
-	var elapsed = end - start; // time in milliseconds
+    var start = Date.now();
+    mutations.forEach(function(mutation) {
+	// console.log(mutation.type);
+    });
+    var end = Date.now();
+    var elapsed = end - start; // time in milliseconds
 
-	//console.log("observer elapsed: " + elapsed / 1000 + " seconds");
-        return elapsed;
+    //console.log("observer elapsed: " + elapsed / 1000 + " seconds");
+    return elapsed;
 });
 
 function purejs_callback(mutations) {
-	var start = Date.now();
-	mutations.forEach(function(mutation) {
-		// console.log(mutation.type);
-	});
-	var end = Date.now();
-	var elapsed = end - start; // time in milliseconds
-
-	//console.log("observer elapsed: " + elapsed / 1000 + " seconds");
-        return elapsed;
+    var start = Date.now();
+    mutations.forEach(function(mutation) {
+	// console.log(mutation.type);
+    });
+    var end = Date.now();
+    var elapsed = end - start; // time in milliseconds
+    
+    //console.log("observer elapsed: " + elapsed / 1000 + " seconds");
+    return elapsed;
 }
 
 // configuration of the observer:
 var config = {
-	attributes: true,
-	childList: true,
-	characterData: true
+    attributes: true,
+    childList: true,
+    characterData: true
 };
 
 
 // CREATE AND MODIFY
-var s = document.createElement("scene");
-document.body.appendChild(s);
-s.setAttribute("id", "testScene");
 var sliderValue = $( "#spinner" ).spinner().spinner( "value" ) ;
 
 var purejs_ents = null;
 
-var createElements = function() {
-	sliderValue = $( "#spinner" ).spinner().spinner( "value" );
-	var start = Date.now();
-        if (!do_dom)
-                purejs_ents = [];
-	for (var i = 0; i < sliderValue; i++) {
-                if (do_dom) {
-		        e = document.createElement("entity");
-		        e.setAttribute("id", i);
-		        s.appendChild(e);
-		        observer.observe(e, config);
-                } else
-                        purejs_ents.push({'id': i, callback: purejs_callback});
-	}
+function createDOMElements(count) {
+    //first create scene, but *not* add to the document yet
+    var s = document.createElement("scene");
+    s.setAttribute("id", "testScene");
 
-	var end = Date.now();
-	var elapsed = end - start; // time in milliseconds
+    //create the elements, add to scene
+    for (var i = 0; i < count; i++) {
+	e = document.createElement("entity");
+	e.setAttribute("id", i);
+	s.appendChild(e);
+	observer.observe(e, config);
+    }
 
-	console.log("create elapsed: " + elapsed / 1000 + " seconds");
+    //add the scene with all the elements to the document body at the end
+    //document.body.appendChild(s);
 }
 
+function createJSElements(count) {
+    purejs_ents = [];
+    for (var i = 0; i < count; i++) {
+	purejs_ents.push({'id': i, callback: purejs_callback});
+    }
+}
+
+var createElements = function() {
+    sliderValue = $( "#spinner" ).spinner().spinner( "value" );
+    var start = Date.now();
+    if (do_dom) {
+	createDOMElements(sliderValue);
+    } else {
+	createJSElements(sliderValue);
+    }
+	
+    var end = Date.now();
+    var elapsed = end - start; // time in milliseconds
+    
+    console.log("create elapsed: " + elapsed / 1000 + " seconds");
+}
+	
 var purejs_modifications = 0;
 var modifyElements = function() {
-	var start = Date.now();
-
-	scene = document.getElementById("testScene");
-	if (do_dom && scene.children.length) {
-                console.log("modifying dom elt");
-		var children = scene.children;
-		for (var i = 0; i < children.length; i++) {
-			var element = children[i];
-			element.setAttribute("id", parseInt(element.getAttribute("id")) + 1);
-		}
+    var start = Date.now();
+    
+    scene = document.getElementById("testScene");
+    if (do_dom && scene.children.length) {
+        console.log("modifying dom elt");
+	var children = scene.children;
+	for (var i = 0; i < children.length; i++) {
+	    var element = children[i];
+	    element.setAttribute("id", parseInt(element.getAttribute("id")) + 1);
 	}
-
-        if (purejs_ents) {
-                for (var i = 0; i < purejs_ents.length; i++) {
-                        var ent = purejs_ents[i];
-                        set_ent_attribute(ent, "id", ent["id"] + 1);
-                        purejs_modifications += 1;
-                }
+    }
+    
+    if (purejs_ents) {
+        for (var i = 0; i < purejs_ents.length; i++) {
+            var ent = purejs_ents[i];
+            set_ent_attribute(ent, "id", ent["id"] + 1);
+            purejs_modifications += 1;
         }
-
-	var end = Date.now();
-	var elapsed = end - start; // time in milliseconds
-
-	//console.log("modify elapsed: " + elapsed / 1000 + " seconds");
-        return elapsed;
+    }
+    
+    var end = Date.now();
+    var elapsed = end - start; // time in milliseconds
+    
+    console.log("modify elapsed: " + elapsed / 1000 + " seconds");
+    return elapsed;
 }
-
+	
 function set_ent_attribute(ent, attrname, val) {
-        ent[attrname] = val;
+    ent[attrname] = val;
 }
-
+	
 /* compat snippet from
    https://developer.mozilla.org/en-US/docs/Web/API/window.requestAnimationFrame */
 // (function() {
